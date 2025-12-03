ac72f5ada9ca8248a9f997a33bd3cfaa5894116c
Stev Wyman <stev.wyman@gmail.com>
2025-12-03 15:07:50 +0100

    test for pipeline

--- beta/tests.py
+++ beta/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
+from models import PingCount
+
 # Create your tests here.
+
+class Ping(TestCase):
+    def test_ping(self) -> None:
+
+        # Get the singleton instance (or create it if it doesn't exist)
+        counter = PingCount.get_singleton()
+
+        # Atomically increment the count in the database
+        new_count = counter.increment_and_save()
+
+        counter = PingCount.get_singleton()
+
+        self.assertEquals(counter.count, 1)
+    
+        print("test completed")
